f85de039665eef4fb50875f41bba1467081f94f0
Dion <kalani.pettaway@gmail.com>
2015-03-04 01:58:13 -0500

    dropped my table like an idiot

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  resources :users
 
   root 'static_pages#home'
+  get 'users/new'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
