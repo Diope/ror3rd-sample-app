5d8c09388c41fcf6e210df5e4f43be038f7d30cb
Dion <kalani.pettaway@gmail.com>
2015-03-26 11:03:08 -0400

    Add user microposts

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,15 @@
+if Rails.env.production?
+	CarrierWave.configure do |config|
+		config.fog_credentials = {
+			# configuratio for amazon s3
+			:provider => 				'AWS',
+			:aws_access_key_id => 		ENV['AKIAJAQYOJ566ULB3ONA']
+			:aws_secret_access_key =>	ENV['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']
+		}
+		config.fog_directory =			ENV['test-oka']
+		
+	end
+
+	Access Key ID:
+
+Secret Access Key:
